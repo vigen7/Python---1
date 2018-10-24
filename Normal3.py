7959cdb28c6954e2bd723927a330a2a6bd5588d8
vigen7 <vigen.osipov@yahoo.com>
2018-10-24 13:31:45 +0300

    ДЗ урок 1

---
+++ Normal3.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      v.osipov
+#
+# Created:     24.10.2018
+# Copyright:   (c) v.osipov 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+print("Введите коэффициенты для квадратного уравнения (ax^2+bx+c=0):")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+discr = b**2-4*a*c
+if discr > 0:
+    import math
+    x1 = (-b+math.sqrt(discr))/(2*a)
+    x2 = (-b-math.sqrt(discr))/(2*a)
+    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+elif discr == 0:
+    x = -b / (2*a)
+    print("x = %.2f" % x)
+else:
+    print("Корней нет")
